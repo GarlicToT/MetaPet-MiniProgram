b8e2908e35544ec8232e1db7f6b8e18e7283e8f8
Garnet <oncwnuKip3WlLIL1nzaYXPeoalJw@git.weixin.qq.com>
2023-06-05 21:09:19 +0800

    add store and community page

--- pages/home_page/home_page.js
+++ pages/home_page/home_page.js
@@ -67,43 +67,7 @@ Page({
 
 Component({
     methods: {
-        navigateToHomePage: function() {
-            wx.navigateTo({
-              url: '/pages/home_page/home_page',
-                events: {
-                  // 为指定事件添加一个监听器，获取被打开页面传送到当前页面的数据
-                  acceptDataFromOpenedPage: function(data) {
-                    console.log(data)
-                  },
-                  someEvent: function(data) {
-                    console.log(data)
-                  }
-                },
-                success: function(res) {
-                  // 通过eventChannel向被打开页面传送数据
-                  res.eventChannel.emit('acceptDataFromOpenerPage', { data: 'test' })
-                }
-              })
-        },
-
-        navigateToStorePage: function() {
-            wx.navigateTo({
-              url: '/pages/store/store',
-                events: {
-                  // 为指定事件添加一个监听器，获取被打开页面传送到当前页面的数据
-                  acceptDataFromOpenedPage: function(data) {
-                    console.log(data)
-                  },
-                  someEvent: function(data) {
-                    console.log(data)
-                  }
-                },
-                success: function(res) {
-                  // 通过eventChannel向被打开页面传送数据
-                  res.eventChannel.emit('acceptDataFromOpenerPage', { data: 'test' })
-                }
-              })
-        },
+        
     },
   });
   
